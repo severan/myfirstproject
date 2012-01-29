b027ce06c78aaf98481a9f4091f4e92ed804b18a
severan@sevbest.com <severan@sevbest.com>
2012-01-28 17:47:12 -0800

    severan third commit

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 require('http').createServer(function(req,res){
     res.writeHead(200,{'Content-Type' : 'text/plain'});
-    res.end('hello\n');
+    res.end('hello 2\n');
 }).listen(process.env.C9_PORT);
